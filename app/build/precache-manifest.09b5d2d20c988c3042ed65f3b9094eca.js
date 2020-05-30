d61687fa7f75252be0513e78be9820fe4fa856c5
Gerald Iakobinyi-Pich <gerald.iakobinyi-pich@inspiricon.de>
2020-05-30 18:17:18 +0300

    Changing folder structure

---
+++ app/build/precache-manifest.09b5d2d20c988c3042ed65f3b9094eca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a898d8abbc70fd7edf901c044c255b34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7156b90c8b373d352b7c",
+    "url": "/static/css/2.bc2ff3bb.chunk.css"
+  },
+  {
+    "revision": "7156b90c8b373d352b7c",
+    "url": "/static/js/2.2c19d963.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/static/js/2.2c19d963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83163aa5990255915f7",
+    "url": "/static/js/main.cad3f035.chunk.js"
+  },
+  {
+    "revision": "0399d1332f4b5f9d63e3",
+    "url": "/static/js/runtime-main.b419a1fa.js"
+  }
+]);
